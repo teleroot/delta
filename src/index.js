241d302acddb890969d98352de315f1f02540815
teleroot <delta.npm@gmail.com>
2018-09-25 15:02:51 +0300

    Add package common index file. Change main script to src/index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export * from "./loader.js";
+export * from "./control-loader.js";
+export * from "./control.js";
+export * from "./utilities/element-options.js";
+export * from "./utilities/indexes";
